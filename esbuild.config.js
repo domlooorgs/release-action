03062776667fae65de60c9d95f9c947fb95940f2
Claycuy <unggaslauncher@gmail.com>
2026-06-15 10:29:42 +0800

    fix: Fix NEW_VERSION bug

---
+++ esbuild.config.js
@@ -0,0 +1,16 @@
+import esbuild from 'esbuild';
+
+async function build() {
+  await esbuild.build({
+    entryPoints: ['src/index.ts', 'src/push-engine.ts'],
+    bundle: true,        // INI PENTING: Masukin semua node_modules ke bundle
+    platform: 'node',    // Targetnya harus node
+    target: 'node20',    // Samain sama versi runner GitHub Action
+    outdir: 'dist',
+    minify: true,
+    sourcemap: false,
+    external: [],        // KOSONGIN: Biar dia "nelen" semua dep ke dalem bundle
+  });
+}
+
+build().catch(() => process.exit(1));
